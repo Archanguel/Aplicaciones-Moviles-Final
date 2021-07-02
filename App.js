78f44bd7ce4b03158f3660c75d6bf57fa32317a1
Alberto Bossio <alberto.federico.bossio@gmail.com>
2021-07-02 02:41:44 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [pokeData, setPokeData] = React.useState(null);
+  const [searchText, setSearchText] = React.useState('');
+
+  function searchPokemon(){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchText.toLowerCase()}`)
+      .then(response => response.json().then(data => setPokeData(data)))
+      .catch(error => console.error(error))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder="Search Pokémon" onChangeText={text => setSearchText(text)} />
+      <Button title="Search" onPress={() => searchPokemon()}/>
+      {pokeData && <Image style={ styles.image } source={{ uri: pokeData.sprites.front_default }} /> }
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200
   },
 });
